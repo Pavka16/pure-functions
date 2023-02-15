5451388f979f88de92ee339f55980574988db0ec
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2023-02-15 22:50:43 +0300

    first commit

---
+++ src/js/__test__/index.test.js
@@ -0,0 +1,11 @@
+import healthStatus from '../app';
+
+test.each([
+  ['Маг', 90, 'healthy'],
+  ['Маг', 50, 'wounded'],
+  ['Маг', 40, 'wounded'],
+  ['Маг', 10, 'critical'],
+])('return health status', (name, health, expected) => {
+  const received = healthStatus({ name, health });
+  expect(received).toBe(expected);
+});
